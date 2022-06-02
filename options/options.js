8fa38cf2084e115b9a137312310c1d0d0090c24e
Crowdbotics <team@crowdbotics.com>
2022-06-02 13:39:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "workmindfulness_35557",
+  url: "https://workmindfulness_35557.botics.co",
+  api: "https://workmindfulness_35557.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
